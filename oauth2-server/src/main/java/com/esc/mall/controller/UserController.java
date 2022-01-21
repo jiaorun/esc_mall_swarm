c6158d426abb3e6964047bf00877028819e09014
jiaorun <18727620519@163.com>
2022-01-21 16:00:28 +0800

    Spring Cloud Security: Oauth2使用入门

---
+++ oauth2-server/src/main/java/com/esc/mall/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.esc.mall.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户 控制层
+ *
+ * @author jiaorun
+ * @date 2022/1/21 11:38
+ **/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+}
