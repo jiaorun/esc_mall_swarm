102d8740bb8a88e7823d5f2194ce4f04507b9bd4
jiaorun <18727620519@163.com>
2022-01-25 12:27:59 +0800

    Spring Cloud Alibaba: Nacos作为注册中心和配置中心使用

---
+++ nacos-config-client/src/main/java/com/esc/mall/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.esc.mall.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 配置 控制层
+ *
+ * @author jiaorun
+ * @date 2022/1/25 12:20
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
